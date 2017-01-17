a198ce62abb457c3278a5c5f8c45688c8e3f678a
Anas Idiab <Candfa2660@gmail.com>
2017-01-17 22:31:38 +0100

    Set up build system

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+using namespace std;
 
 int main(int argc, char* argv[]){
-  
+  SDL_Init(SDL_INIT_EVERYTHING);
+  int a = 5;
+  cout << "The program was run with the following command:\n\t" << argv[0] << "\n";
+  cout << "5 + N[arguments] = " << a + argc << "\n";
+  SDL_Quit();
   return 0;
 }
-
 
